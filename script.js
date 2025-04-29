37bacd7d695c39b8c975de8b780c11e4bc0cc976
AydenOcceas <aydenocceas@gmail.com>
2025-04-29 13:26:56 -0700

    Add update game functionality
    create a system for updating the game after the play round is called by
    the new button event listener

--- script.js
+++ script.js
@@ -1,56 +1,36 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 
-// playGame();
+let scoreCounter = document.querySelector("#score")
+let playInfo = document.querySelector("#playerInfo");
 
-function playGame(){
+function updateGame(){
 
-    for (let i = 0; i < 5; i++){
-        playRound(getUserChoice(), getComputerChoice());
-        console.log(`Player: ${humanScore} Computer: ${computerScore}. Next round!`);
-    }
+    scoreCounter.textContent = `Player: ${humanScore} || Computer: ${computerScore}. Next round!`;
 
-    if (humanScore === computerScore){
-        console.log("It's a tie, better luck next time.");
+    //updates round counter until the game is over
+    if (round < 5) {
+        round++;
+    } else if (humanScore === computerScore){ //if the round is over evaluate who won
+        playInfo.textContent = "It's a tie, better luck next time.";
+        resetGame();
     } else if (humanScore > computerScore) {
-        console.log("You win! Congratulations");
+        playInfo.textContent = "You win! Congratulations";
+        resetGame();
     } else {
-        console.log("you lose. womp.");
-    }
-
-    console.log(`Player: ${humanScore} Computer: ${computerScore}`);
-}
-
-
-function playRound(humanChoice, computerChoice) {
-    const humanVal = getChoiceVal(humanChoice);
-    const computerVal = getChoiceVal(computerChoice);
-
-    // switch case on full set of game states
-    switch (humanVal - computerVal) {
-        // tie case
-        case 0:
-            console.log("It's a tie! You'll get em\' next time.");
-            break;
-        
-        // win cases
-        case 1:
-        case -2:
-            console.log(`you win! ${humanChoice} beats ${computerChoice}!`);
-            humanScore++;
-            break;
-        
-        // lose cases
-        case 2:
-        case -1:
-            console.log(`you lose! ${computerChoice} beats ${humanChoice}. Better luck next time.`);
-            computerScore++;
-            break;
-
-        default:
-            console.log("It's a tie! You'll get em\' next time.");
+        playInfo.textContent = "you lose. womp.";
+        resetGame();
     }
 }
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+    scoreCounter.textContent = `Player: ${humanScore} || Computer: ${computerScore}`;
+}
+
 
 // assigns each choice a numerical value
 function getChoiceVal(choice) {
@@ -69,6 +49,40 @@ function getChoiceVal(choice) {
     }
 }
 
+// contains RPS logic
+function playRound(humanChoice, computerChoice) {
+    const humanVal = getChoiceVal(humanChoice);
+    const computerVal = getChoiceVal(computerChoice);
+
+    // switch case on full set of game states
+    switch (humanVal - computerVal) {
+        // tie case
+        case 0:
+            playInfo.textContent = "It's a tie! You'll get em\' next time.";
+            updateGame();
+            break;
+        
+        // win cases
+        case 1:
+        case -2:
+            playInfo.textContent = `you win! ${humanChoice} beats ${computerChoice}!`;
+            humanScore++;
+            updateGame();
+            break;
+        
+        // lose cases
+        case 2:
+        case -1:
+            playInfo.textContent = `you lose! ${computerChoice} beats ${humanChoice}. Better luck next time.`;
+            computerScore++;
+            updateGame();
+            break;
+
+        default:
+            playInfo.textContent = "It's a tie! You'll get em\' next time.";
+            updateGame();
+    }
+}
 
 // use Event Delegation to get user choice from buttons
 let choices = document.querySelector('ul');
